64f4624648bb587574a605f2b0f45a698e7fa766
Gyroid <jockm24@gmail.com>
2025-02-17 23:19:23 +0000

    added method to generate random integers

---
+++ PandorasCodeBox/PandorasCodeBox/RNG/LinkedRNG.cs
@@ -0,0 +1,43 @@
+﻿using PandorasCodeBox.Loops;
+
+namespace PandorasCodeBox.RNG;
+
+public static unsafe class LinkedRNG
+{
+    private struct Node
+    {
+        public fixed byte Data[16];
+    }
+    
+    public static int Random(int min, int max, int probability)
+    {
+        var nodes = stackalloc Node[max - min];
+        
+        {
+            int i = 0;
+            
+            LOOP_START:
+
+            *(int*)(nodes->Data + i * 16) = i;
+            *(Node**)(nodes->Data + i * 16 + 8) = &nodes[((i + 1) % (max - min)) * 16]; // man this line is dumb
+            
+            if (++i < max - min)
+            {
+                goto LOOP_START;
+            }
+        }
+
+        return min + Roll(probability, nodes);
+        
+        int Roll(int probability, Node* node)
+        {
+            if (DateTime.UtcNow.Second % probability == 0)
+            {
+                return probability;
+            }
+
+            Node* nextNode = *(Node**)(node->Data + 8);
+            return Roll(probability, nextNode);
+        }
+    }
+}
